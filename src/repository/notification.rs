5ce0f98ff486d65a15af28b8e71f3bd0924348e6
joshcr25 <Josh Christmas Rommlynn>
2026-03-28 22:50:05 +0700

    Implement list_all_as_string function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -1,20 +1,28 @@
-use std:: sync:: RwLock;
+use std::sync::RwLock;
 
-use lazy_static :: lazy_static;
+use lazy_static::lazy_static;
 
-use crate:: model :: notification :: Notification;
+use crate::model::notification::Notification;
 
 // Singleton of Database
 lazy_static! {
-static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock :: new(vec! []);
+    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
 }
 
 pub struct NotificationRepository;
 
 impl NotificationRepository {
-    pub fn add (notification: Notification) -> Notification {
-        NOTIFICATIONS.write(). unwrap()
-        .push(notification.clone());
+    pub fn add(notification: Notification) -> Notification {
+        NOTIFICATIONS.write().unwrap().push(notification.clone());
         return notification;
+    }
+
+    pub fn list_all_as_string() -> Vec<String> {
+        return NOTIFICATIONS
+            .read()
+            .unwrap()
+            .iter()
+            .map(|f| format!("{}", f.clone()))
+            .collect();
     }
 }
